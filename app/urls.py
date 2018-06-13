54e7ae6d900d4fe90406ffb492940418da9ebaeb
kautukkundan <kautukkundan@gmail.com>
2018-06-13 23:37:02 +0530

    url fix

--- app/urls.py
+++ app/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 from django.urls import include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'',include('blog.urls'))
+    url('admin/', admin.site.urls),
+    url(r'',include('blog.urls'))
 ]
